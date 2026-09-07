db7728bb5681fa078a7e245c34204f2d5b293d1d
DWYANE SIDO <C202401138@iacademy.edu.ph>
2026-09-07 14:10:59 +0800

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,62 @@
-import Image from "next/image";
+import Image from 'next/image'
+import heroImg1 from '../public/hero-image-main.png';
+import heroImg2 from '../public/hero-image-small-two.png';
+import heroImg3 from '../public/hero-image-small-one.png';
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <main>
+      <section className="hero-section">
+        <h1 className="hero-title">
+          <span className="hero-sans">Hello, I&apos;m </span>
+          <span className="hero-serif">Dwyane. </span>
+        </h1>
+        <h2 className="heading">
+          <span className="heading-serif">Visual Artist / </span>
+          <span className="heading-sans">Software Engineer</span>
+          <span className="heading-serif"> / Graphics Designer</span>
+        </h2>
+
+        <div className="hero-images">
+          <div className="hero-images-1">
             <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
+              src={heroImg1}
+              alt="hero image"
+              placeholder="blur"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+          <div className="hero-images-2">
+            <Image
+              src={heroImg2}
+              alt="hero image"
+              placeholder="blur"
+            />
+          </div>
+          <div className="hero-images-3">
+            <Image
+              src={heroImg3}
+              alt="hero image"
+              placeholder="blur"
+            />
+          </div>
+          <div className="hero-images-4"></div>
+          <div className="hero-images-5"></div>
         </div>
-      </main>
-    </div>
+
+        <div className="hero-text">
+          <h3>Welcome to my website.</h3>
+          <p>
+            I’m currently a 20 y.o. college student learning the wonderfully
+            volatile life of a software engineer.
+          </p>
+          <p>
+            Aspiring to become a professional in the sphere of computer
+            technology, I’m hoping to earn a sizeable enough living to laze
+            around all day without regret.
+          </p>
+          <p className="text-light">I hope you enjoy your time here.</p>
+        </div>
+      </section>
+    </main>
   );
 }
